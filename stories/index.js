fd5593526dccd775d71e81399eb70114b26e7914
sbentley2019 <sbentley000@gmail.com>
2019-11-26 17:12:19 -0500

    Create stories for the Appointments components.

--- stories/index.js
+++ stories/index.js
@@ -101,4 +101,42 @@ storiesOf("Button", module)
         avatar={interviewer.avatar}
         setInterviewer={event => action("setInterviewer")(interviewer.id)}
       />
-    ));
+    ))
+
+
+    storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time="12pm" />)
+    .add("Header", () => <Header time="12pm" />)
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => {
+    return (
+      <Show 
+        student="Lydia Miller-Jones"
+        interviewer={interviewer}
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
+      />
+    );
+  })
+    .add("Confirm", () => {
+      return(
+      <Confirm 
+        message="Delete the appointment?"
+        onConfirm={action("onConfirm")}
+        onCancel={action("onCancel")}
+      />
+      )
+    })
+    .add("Status", () => <Status message="Deleting" />)
+    .add("Error", () => {
+    return (
+      <Error 
+        message="Could not delete appointment." 
+        onClose={action("onClose")} 
+      />
+    );
+    });
