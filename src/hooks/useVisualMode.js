2c070a90afd5dcf89ac3f9f32425e989d5a42d6d
sbentley2019 <sbentley000@gmail.com>
2019-11-27 18:22:52 -0500

    Create custom hook useVisualMode to transition forward or back mode.

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function(mode, replace = false) {
+    if (replace) {
+      setHistory(prevHistory => prevHistory.slice(0, history.length - 1));
+    }
+    setHistory(prevHistory => [...prevHistory, mode]);
+    setMode(mode);
+    return { transition };
+  }
+
+  const back = function() {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory(prevHistory => prevHistory.slice(0, history.length - 1));
+    }
+    return { back };
+  }
+
+  return { mode, transition, back };
+};
