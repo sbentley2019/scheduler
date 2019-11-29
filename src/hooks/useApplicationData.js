707cd4c453030487e6088171994f635ddc556f98
sbentley2019 <sbentley000@gmail.com>
2019-11-29 13:18:40 -0500

    Implement useReducer instead of useState.

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,15 +1,30 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+const reducer = function(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return {...state, day: action.value};
+    case SET_APPLICATION_DATA:
+      const arr = action.value;
+      return {...state, days: arr[0].data, appointments: arr[1].data, interviewers: arr[2].data};
+    case SET_INTERVIEW:
+      return { ...state, appointments: action.value};
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
 
 export default function useApplicationData() {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
+  const [state, dispatch] = useReducer(reducer, {day: "Monday", days: [], appointments: {}, interviewers: {}});
   
-  const setDay = day => setState({ ...state, day});
+  const setDay = day => dispatch({type: SET_DAY, value: day});
   
   useEffect(() => {
     Promise.all([
@@ -17,7 +32,7 @@ export default function useApplicationData() {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+      dispatch({type: SET_APPLICATION_DATA, value: all});
     });
   }, [])
   
@@ -32,7 +47,7 @@ export default function useApplicationData() {
     };
     return axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
     .then(() => {
-      setState({ ...state, appointments});
+      dispatch({type: SET_INTERVIEW, value: appointments});
     });
   };
   
@@ -43,7 +58,7 @@ export default function useApplicationData() {
     };
     return axios.delete(`http://localhost:8001/api/appointments/${id}`)
     .then(() => {
-      setState({ ...state, appointments})
+      dispatch({type: SET_INTERVIEW, value: appointments});
     })
   };
 
