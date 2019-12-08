d90f25d6a1576667077a9ac48aacaa564b75b637
sbentley2019 <sbentley000@gmail.com>
2019-12-08 17:15:33 -0500

    Add form check when submitting an appointment without an interviewer selected.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,6 +23,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (interviewer === null) {
+      setError("Interviewer needs to be selected");
+      return;
+    }
   
     setError("");
     props.onSave(name, interviewer);
