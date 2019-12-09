f67262fc3ce91c0ebdf97a4a308aa75946ad0824
sbentley2019 <sbentley000@gmail.com>
2019-12-08 21:16:04 -0500

    Fix bugs and refactor code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,16 +1,14 @@
 const getAppointmentsByDay = function(state, day) {
   // A function that returns an array of appointments for a specific day
-  let appoints = [];
-  for (let appDay of state.days) {
-    if (appDay.name === day) {
-      appoints = appDay.appointments;
-    }
+  let appoints = state.days.find(appDay => appDay.name === day);
+  if (!appoints) {
+    return [];
   }
+  appoints = appoints.appointments;
 
   const result = [];
-  for (let index of appoints) {
-    result.push(state.appointments[index]);
-  }
+  appoints.map(index => result.push(state.appointments[index]));
+
   return result;
 };
 
@@ -26,17 +24,15 @@ const getInterview = function(state, interview) {
 
 const getInterviewersByDay = function(state, day) {
   // A function that returns an array of interviewers for a specified day
-  let availInterviewers = [];
-  for (let value of state.days) {
-    if (value.name === day) {
-      availInterviewers = value.interviewers;
-    }
+  let availInterviewers = state.days.find(value => value.name === day);
+  if (!availInterviewers) {
+    return [];
   }
+  availInterviewers = availInterviewers.interviewers;
 
   const result = [];
-  for (let index of availInterviewers) {
-    result.push(state.interviewers[index]);
-  }
+  availInterviewers.map(index => result.push(state.interviewers[index]));
+
   return result;
 };
 
